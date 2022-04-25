6c4336a74d7dc4a48f06902fdd617aac184db761
Michaja Pehl <pehl@pik-potsdam.de>
2022-04-25 16:06:00 +0200

    add renv to files/directories to copy to output

--- scripts/start/submit.R
+++ scripts/start/submit.R
@@ -20,7 +20,7 @@
 ############## Define function: runsubmit #########################
 
 submit <- function(cfg, restart = FALSE) {
-  
+
   if(!restart) {
     # Generate name of output folder and create the folder
     date <- format(Sys.time(), "_%Y-%m-%d_%H.%M.%S")
@@ -37,30 +37,31 @@ submit <- function(cfg, restart = FALSE) {
       unlink(cfg$results_folder, recursive = TRUE)
       dir.create(cfg$results_folder, recursive = TRUE, showWarnings = FALSE)
     }
-    
+
     # remember main folder
     cfg$remind_folder <- getwd()
-    
-    # Save the cfg (with the updated name of the result folder) into the results folder. 
-    # Do not save the new name of the results folder to the .RData file in REMINDs main folder, because it 
+
+    # Save the cfg (with the updated name of the result folder) into the results folder.
+    # Do not save the new name of the results folder to the .RData file in REMINDs main folder, because it
     # might be needed to restart subsequent runs manually and should not contain the time stamp in this case.
     filename <- paste0(cfg$results_folder,"/config.Rdata")
     cat("   Writing cfg to file",filename,"\n")
     save(cfg,file=filename)
-    
+
     # Copy files required to configure and start a run
     filelist <- c("prepare_and_run.R" = "scripts/start/prepare_and_run.R",
-                  ".Rprofile" = ".Rprofile")
+                  ".Rprofile" = ".Rprofile",
+                  'renv')
     .copy.fromlist(filelist,cfg$results_folder)
-    
+
     # Do not remove .RData files from REMIND main folder because they are needed in case you need to manually restart subsequent runs.
   }
-  
+
   # Change to run folder
   setwd(cfg$results_folder)
   on.exit(setwd(cfg$remind_folder))
-  
-  # send prepare_and_run.R to cluster 
+
+  # send prepare_and_run.R to cluster
   cat("   Executing prepare_and_run.R for",cfg$results_folder,"\n")
   if(cfg$slurmConfig=="direct") {
     log <- format(Sys.time(), paste0(cfg$title,"-%Y-%H-%M-%S-%OS3.log"))
@@ -69,6 +70,6 @@ submit <- function(cfg, restart = FALSE) {
     system(paste0("sbatch --job-name=",cfg$title," --output=log.txt --mail-type=END --comment=REMIND --wrap=\"Rscript prepare_and_run.R \" ",cfg$slurmConfig))
     Sys.sleep(1)
   }
-    
+
   return(cfg$results_folder)
 }
