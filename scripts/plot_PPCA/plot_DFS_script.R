d317a209c0a83a7e5e524b727c6116a7db2999e9
Steve Bi <stephen.bi@pik-potsdam.de>
2022-09-29 11:28:34 +0200

    Add plotting scripts for bubble charts

---
+++ scripts/plot_PPCA/plot_DFS_script.R
@@ -0,0 +1,34 @@
+
+source("./scripts/input/COALogit_PPCA.R")
+
+# COALogit_PPCA(refgdx=NULL,recovery="BAU",size="2p",PPCA_pol="power",oecd="on",nonoecd="off",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_power-current-cfC52_teCost_julyMem_solfix-BAU.mif",plot="DFS_2045_Neutral_power_50p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="BAU",size="1p",PPCA_pol="power",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_power-OECD-1p-cfC52_teCost_julyMem_solfix-BAU.mif",plot="DFS_2045_Neutral_power_95p_rev_rd2_noEU.png")
+# COALogit_PPCA(refgdx=NULL,recovery="BAU",size="2p",PPCA_pol="power",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_power-OECD-2p-cfC52_teCost_julyMem_solfix-BAU.mif",plot="DFS_2045_Neutral_power_50p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="BAU",size="3p",PPCA_pol="power",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_power-OECD-3p-cfC52_teCost_julyMem_solfix-BAU.mif",plot="DFS_2045_Neutral_power_5p_rev_rd2_noEU.png")
+
+COALogit_PPCA(refgdx=NULL,recovery="Brown",size="2p",PPCA_pol="power",oecd="on",nonoecd="off",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_power-current-cfC52_teCost_julyMem_solfix-Brown.mif",plot="DFS_2045_Brown_power_50p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="Brown",size="1p",PPCA_pol="power",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_power-OECD-1p-cfC52_teCost_julyMem_solfix-Brown.mif",plot="DFS_2045_Brown_power_95p_rev_rd2_noEU.png")
+# COALogit_PPCA(refgdx=NULL,recovery="Brown",size="2p",PPCA_pol="power",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_power-OECD-2p-cfC52_teCost_julyMem_solfix-Brown.mif",plot="DFS_2045_Brown_power_50p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="Brown",size="3p",PPCA_pol="power",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_power-OECD-3p-cfC52_teCost_julyMem_solfix-Brown.mif",plot="DFS_2045_Brown_power_5p_rev_rd2_noEU.png")
+
+COALogit_PPCA(refgdx=NULL,recovery="Green",size="2p",PPCA_pol="power",oecd="on",nonoecd="off",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_power-current-cfC52_teCost_julyMem_solfix-Green.mif",plot="DFS_2045_Green_power_50p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="Green",size="1p",PPCA_pol="power",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_power-OECD-1p-cfC52_teCost_julyMem_solfix-Green.mif",plot="DFS_2045_Green_power_95p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="Green",size="2p",PPCA_pol="power",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_power-OECD-2p-cfC52_teCost_julyMem_solfix-Green.mif",plot="DFS_2045_Green_power_50p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="Green",size="3p",PPCA_pol="power",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_power-OECD-3p-cfC52_teCost_julyMem_solfix-Green.mif",plot="DFS_2045_Green_power_5p_rev_rd2_noEU.png")
+
+
+COALogit_PPCA(refgdx=NULL,recovery="BAU",size="2p",PPCA_pol="demand",oecd="on",nonoecd="off",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_demand-current-cfC52_teCost_julyMem_solfix-BAU.mif",plot="DFS_2045_Neutral_demand_50p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="BAU",size="1p",PPCA_pol="demand",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_demand-OECD-1p-cfC52_teCost_julyMem_solfix-BAU.mif",plot="DFS_2045_Neutral_demand_95p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="BAU",size="2p",PPCA_pol="demand",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_demand-OECD-2p-cfC52_teCost_julyMem_solfix-BAU.mif",plot="DFS_2045_Neutral_demand_50p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="BAU",size="3p",PPCA_pol="demand",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_demand-OECD-3p-cfC52_teCost_julyMem_solfix-BAU.mif",plot="DFS_2045_Neutral_demand_5p_rev_rd2_noEU.png")
+
+COALogit_PPCA(refgdx=NULL,recovery="Brown",size="2p",PPCA_pol="demand",oecd="on",nonoecd="off",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_demand-current-cfC52_teCost_julyMem_solfix-Brown.mif",plot="DFS_2045_Brown_demand_50p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="Brown",size="1p",PPCA_pol="demand",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_demand-OECD-1p-cfC52_teCost_julyMem_solfix-Brown.mif",plot="DFS_2045_Brown_demand_95p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="Brown",size="2p",PPCA_pol="demand",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_demand-OECD-2p-cfC52_teCost_julyMem_solfix-Brown.mif",plot="DFS_2045_Brown_demand_50p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="Brown",size="3p",PPCA_pol="demand",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_demand-OECD-3p-cfC52_teCost_julyMem_solfix-Brown.mif",plot="DFS_2045_Brown_demand_5p_rev_rd2_noEU.png")
+
+COALogit_PPCA(refgdx=NULL,recovery="Green",size="2p",PPCA_pol="demand",oecd="on",nonoecd="off",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_demand-current-cfC52_teCost_julyMem_solfix-Green.mif",plot="DFS_2045_Green_demand_50p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="Green",size="1p",PPCA_pol="demand",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_demand-OECD-1p-cfC52_teCost_julyMem_solfix-Green.mif",plot="DFS_2045_Green_demand_95p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="Green",size="2p",PPCA_pol="demand",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_demand-OECD-2p-cfC52_teCost_julyMem_solfix-Green.mif",plot="DFS_2045_Green_demand_50p_rev_rd2_noEU.png")
+COALogit_PPCA(refgdx=NULL,recovery="Green",size="3p",PPCA_pol="demand",oecd="off",nonoecd="on",outputfolder="plots",title=NULL,run="./finalScenMifs/REMIND_generic_PPCA_demand-OECD-3p-cfC52_teCost_julyMem_solfix-Green.mif",plot="DFS_2045_Green_demand_5p_rev_rd2_noEU.png")
+
