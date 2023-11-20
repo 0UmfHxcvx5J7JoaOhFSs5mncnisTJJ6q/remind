e686270e71bb3b6bff169fd5ca8d715556b24fc6
Michaja Pehl <pehl@pik-potsdam.de>
2023-11-20 13:35:02 +0100

    adjust script from lowEn to highME

--- scripts/make-lowEn-mod.R
+++ scripts/make-lowEn-mod.R
@@ -95,13 +95,13 @@ write_csv(
 
 system(paste('tar --create --gzip --file',
              sub('^(.*rev[0-9\\.]+)_(.*)$',
-                 'calibration_results/\\1-lowEn-mod_\\2',
+                 'calibration_results/\\1-highME-mod_\\2',
                  basename(idr_tar)),
              '-C', tar_dir,
              paste(list.files(tar_dir), collapse = ' ')))
 
 file.copy(sub('_remind.tgz$', '_validationremind.tgz', idr_tar),
           sub('^.*(rev[0-9\\.]+)_([0-9a-f]{8})_remind.tgz',
-              'calibration_results/\\1-lowEn-mod_\\2_validationremind.tgz',
+              'calibration_results/\\1-highME-mod_\\2_validationremind.tgz',
               idr_tar),
           overwrite = TRUE)
