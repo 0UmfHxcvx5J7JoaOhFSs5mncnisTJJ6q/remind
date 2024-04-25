6e4a843ffc4446e763d0f24bfaa1544407b1259d
Michaja Pehl <pehl@pik-potsdam.de>
2024-04-25 12:01:49 +0200

    bump input revision number

--- scripts/make-highME-mod.R
+++ scripts/make-highME-mod.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(assertr)
 
-base_path <- '/p/projects/rd3mod/inputdata/output/rev6.75-indccslim2'
+base_path <- '/p/projects/rd3mod/inputdata/output/rev6.75-indccslim3'
 idr_tar <- list.files(dirname(base_path),
                       paste0(basename(base_path), '_62eff8f7_remind.tgz$'),
                       full.names = TRUE)
